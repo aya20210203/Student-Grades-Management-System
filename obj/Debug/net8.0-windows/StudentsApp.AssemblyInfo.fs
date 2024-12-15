8f446f349fee89256a091ce8c7ef29bdb0cf1c75
Kshishtawy <kareememadelshishtawy@gmail.com>
2024-12-15 11:32:05 +0200

    Adding system.forms reference
    حمو الديلر

--- obj/Debug/net8.0-windows/StudentsApp.AssemblyInfo.fs
+++ obj/Debug/net8.0-windows/StudentsApp.AssemblyInfo.fs
@@ -7,11 +7,13 @@ open System
 open System.Reflection
 
 
-[<assembly: System.Reflection.AssemblyCompanyAttribute("student_grades_management_system")>]
+[<assembly: System.Reflection.AssemblyCompanyAttribute("StudentsApp")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
 [<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cd45735095a73d2a4a058dec65f509a7aa5c540")>]
-[<assembly: System.Reflection.AssemblyProductAttribute("student_grades_management_system")>]
-[<assembly: System.Reflection.AssemblyTitleAttribute("student_grades_management_system")>]
+[<assembly: System.Reflection.AssemblyProductAttribute("StudentsApp")>]
+[<assembly: System.Reflection.AssemblyTitleAttribute("StudentsApp")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
+[<assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")>]
+[<assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")>]
 do()
